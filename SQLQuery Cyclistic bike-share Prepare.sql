af863c491700389dc26d950ca22bba6f324c37de
RizkyDwiH <rizkydwi1234ok@gmail.com>
2025-02-11 14:37:44 +0700

    Add files via upload

---
+++ SQLQuery Cyclistic bike-share Prepare.sql
@@ -0,0 +1,113 @@
+/*
+
+Prepare and Combine Data
+
+*/
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202401]
+UNION ALL
+
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202402]
+UNION ALL
+
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202403]
+UNION ALL
+
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202404]
+UNION ALL
+
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202405]
+UNION ALL
+
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202406]
+UNION ALL
+
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202407]
+UNION ALL
+
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202408]
+UNION ALL
+
+
+SELECT 
+ride_id,
+rideable_type,
+started_at,
+ended_at,
+start_station_name,
+end_station_name,
+member_casual
+FROM [Cyclistic bike-share].[dbo].[202409]
+
+
